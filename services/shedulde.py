932a566f91c6c8acb8c47e9031050767de3d643e
unknown <Bystrov Roman>
2023-02-28 14:53:41 +0300

    Хелпер функция дял определения завтрешней даты

--- services/shedulde.py
+++ services/shedulde.py
@@ -1,14 +1,28 @@
 from datetime import date
+from calendar import monthrange
 
 from nsmu_parser.parser import get_list
 
 
 def shedulde_by_group(link: str, shedulde_time: str):
     today_date = date.today()
-    tomoroww_date = date(today_date.year, today_date.month, today_date.day + 1)
+    tomorow_date = get_tomorrow_date(today_date)
 
     if shedulde_time == 'today':
         return get_list(today_date, link)
 
     if shedulde_time == 'tomorrow':
-        return get_list(tomoroww_date, link)
+        return get_list(tomorow_date, link)
+
+
+def get_tomorrow_date(today: date) -> date:
+    weekday, days_in_month = monthrange(today.year, today.month)
+
+    tomorrow_day: int = today.day + 1
+    month: int = today.month
+
+    if tomorrow_day > days_in_month:
+        tomorrow_day = 1
+        month += 1
+
+    return date(today.year, month, tomorrow_day)
